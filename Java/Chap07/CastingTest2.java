55330d1092b42b71112c2fb7a24988b8544a4881
NB <nash4comp@gmail.com>
2023-06-08 08:28:25 -0700

    Test of polymorphism of Java

---
+++ Java/Chap07/CastingTest2.java
@@ -0,0 +1,34 @@
+package Chap07;
+
+public class CastingTest2 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Car car2 = null;
+        FireEngine fe = null;
+
+        car.drive();
+        fe = (FireEngine) car; // Runtime Error
+        fe.drive();
+        car2 = fe;
+        car2.drive();
+    }
+}
+
+class Car {
+    String color;
+    int door;
+
+    void drive() {
+        System.out.println("drive, Brrr~");
+    }
+
+    void stop() {
+        System.out.println("stop!!!");
+    }
+}
+
+class FireEngine extends Car {
+    void water() {
+        System.out.println("water!!!");
+    }
+}
