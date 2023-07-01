b8cc0c237ef8dcd1eb89f8690134324a3e142086
NB <nash4comp@gmail.com>
2023-07-01 00:28:10 -0700

    Practice stream for Java

---
+++ Java/Practice/StreamEx05.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import java.util.*;
+
+// Write a Java program to count the number of strings in a list that start with a specific letter using streams.
+
+public class StreamEx05 {
+    public static void main(String[] args) {
+        List<String> strArr = Arrays.asList("harry", "jane", "tom", "Brian", "jean", "jack", "nash", "jacky");
+        long num = strArr.stream()
+                .filter(s -> s.startsWith(args[0]))
+                .count();
+        System.out.println(num);
+    }
+}
---
+++ Java/Practice/StreamEx06.java
@@ -0,0 +1,23 @@
+package Practice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+// Write a Java program to sort a list of strings in alphabetical order, ascending and descending using streams.
+public class StreamEx06 {
+    public static void main(String[] args) {
+        List<String> strLst = Arrays.asList("harry", "jane", "tom", "Brian", "jean", "jack", "nash", "jacky");
+        List<String> ascendingOrdered = strLst.stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(ascendingOrdered);
+
+        List<String> descendingOrdered = strLst.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        System.out.println(descendingOrdered);
+
+    }
+}
