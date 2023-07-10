802e2869ced012e81f05c082c60963c5846b3c98
NB <nash4comp@gmail.com>
2023-07-09 18:52:37 -0700

    Practice stream medium problems

---
+++ Java/Practice/Stream/Ex02.java
@@ -0,0 +1,30 @@
+// ChatGPT generated problems #1
+
+// Write a method that returns the average age of all users in a list.
+// Assume a User class with name and age fields.
+
+import java.util.*;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        List<UserData> users = Arrays.asList(
+                new UserData("John", 30),
+                new UserData("Alice", 45),
+                new UserData("Bob", 20),
+                new UserData("Charlie", 54),
+                new UserData("Diana", 27),
+                new UserData("Evan", 35),
+                new UserData("Frank", 41),
+                new UserData("Tina", 52));
+
+        OptionalDouble average = users.stream()
+                .mapToInt(UserData::getAge)
+                .average();
+
+        if (average.isPresent()) {
+            System.out.println("Average age: " + average.getAsDouble());
+        } else {
+            System.out.println("No data found");
+        }
+    }
+}
---
+++ Java/Practice/Stream/UserData.java
@@ -0,0 +1,17 @@
+public class UserData {
+    private String name;
+    private int age;
+
+    public UserData(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
