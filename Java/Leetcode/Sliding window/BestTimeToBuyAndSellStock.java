6d9f5bfba104940b3058a493199aa282c3531d9e
Nash Baek <nash4comp@gmail.com>
2023-07-11 13:34:54 -0700

    Solving sliding window problem in leetcode

---
+++ Java/Leetcode/Sliding window/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        // find min value and index in the given array
+        int minValue = -1;
+        int minIndex = -1;
+        int result = 0;
+
+        // List<Integer> numArray = Arrays.asList(prices);
+        // result = numArray.size();
+
+        // find the max value and index in the given array, starting from the index after finding the min index
+
+        return result;
+    }
+}
